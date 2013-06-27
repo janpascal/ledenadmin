2fa7d89e26db954ac68f386a10be718b5eafef9b
Jan-Pascal van Best <janpascal@vanbest.org>
2013-06-27 21:29:56 +0200

    Release 0.6.1

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import play.Project._
 object ApplicationBuild extends Build {
 
   val appName         = "ledenadmin"
-  val appVersion      = "0.6.0"
+  val appVersion      = "0.6.1"
 
   val appDependencies = Seq(
     // Add your project dependencies here,

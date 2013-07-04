f750c91ed466f91d740558d9d8c5f7562c2bdafe
Jan-Pascal van Best <janpascal@vanbest.org>
2013-07-04 22:33:18 +0200

    Added play-easymail dependency instead of commons-email

--- project/Build.scala
+++ project/Build.scala
@@ -14,11 +14,12 @@ object ApplicationBuild extends Build {
     javaEbean,
     "net.sf.opencsv" % "opencsv" % "2.3",
     "mysql" % "mysql-connector-java" % "5.1.18",
-    "org.apache.commons" % "commons-email" % "1.3.1"
+    "com.feth" %% "play-easymail" % "0.2-SNAPSHOT"
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
+      resolvers += Resolver.url("play-easymail (release)", url("http://joscha.github.com/play-easymail/repo/releases/"))(Resolver.ivyStylePatterns),
+      resolvers += Resolver.url("play-easymail (snapshot)", url("http://joscha.github.com/play-easymail/repo/snapshots/"))(Resolver.ivyStylePatterns)
   )
 
 }

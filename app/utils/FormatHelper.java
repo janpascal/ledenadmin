bccaf2dd163758c2d42d4e51cbb32a517bf9822f
Jan-Pascal van Best <janpascal@vanbest.org>
2013-09-17 20:33:18 +0200

    Fix null pointer exception on displaying null currency values

--- app/utils/FormatHelper.java
+++ app/utils/FormatHelper.java
@@ -6,6 +6,7 @@ import java.text.DecimalFormat;
 public class FormatHelper {
 
     public static String formatCurrency(BigDecimal value, String format) {
+        if(value == null) return "";
         DecimalFormat formatter = new DecimalFormat(format);
         return formatter.format(value.doubleValue());
    }

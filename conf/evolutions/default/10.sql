befe7534ffc2353cc02ade95a306fb8e3906986f
Jan-Pascal van Best <janpascal@vanbest.org>
2013-09-17 20:31:43 +0200

    Also allow decimals in amounts in invoices

---
+++ conf/evolutions/default/10.sql
@@ -0,0 +1,13 @@
+# --- !Ups
+
+
+alter table factuur modify bedrag decimal(38,2) not null
+
+# --- !Downs
+
+SET FOREIGN_KEY_CHECKS=0;
+
+alter table factuur modify bedrag decimal(38) not null
+
+SET FOREIGN_KEY_CHECKS=1;
+
